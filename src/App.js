faf5084ca4f34a1d87161fe4ee7e73001854da70
Dharisini <dharisini@besquare.com.my>
2022-07-17 21:26:06 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import Axios from "axios";
+import React, { useState } from "react";
+// import React from "react";
+import "./App.css";
+
+// const url = " ";
 
 function App() {
+  const [comments, setCommentsl] = useState([]);
+  // const [, setEmail] = useState("");
+
+  const getComment = () => {
+    Axios.get("https://jsonplaceholder.typicode.com/comments").then(
+      (response) => {
+        //console.log(response.data);
+        setCommentsl(response?.data);
+      }
+    );
+// ? operational chaining 
+    // const createPost = () => {
+    //   Axios.get("https://jsonplaceholder.typicode.com/comments/1").then(
+    //     (response) => {
+    //       console.log(response.data);
+    //     }
+    //   );
+    //     Axios.post("https://jsonplaceholder.typicode.com/comments", {
+    //       title: "why la?",
+    //     }).then(response.data);
+    // };
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Helloooo</h1>
+      <button onClick={getComment}> Fetch that email!</button>
+      {/* {email} */}
+      {
+        /* <button2 onClick={createPost}> create that!</button2> */
+        // comments.forEach(
+        //   (comment) => {
+        //     console.log(comment);
+        //   }
+        // )
+
+        comments.map((comment) => {
+          return <p>{comment.email}</p>;
+        })
+      }
     </div>
   );
 }
